8c9dbb29e2979176c64e639b16f64daff9625c68
wegzo <rope.ropelli@gmail.com>
2016-12-05 17:47:39 +0200

    Update abnf_parser.cpp

--- abnf_parser.cpp
+++ abnf_parser.cpp
@@ -156,12 +156,12 @@ bool abnf_element::generate(str_const_iterator& it, const str_const_iterator& en
     return false;
 }
 
-bool abnf_element::run(str_const_iterator& it, const str_const_iterator& end)
+bool abnf_element::run(str_const_iterator& it, const str_const_iterator& end, matched_patterns_t& r) const
 {
     assert(this->element.get());
 
     str_const_iterator jt = it;
-    bool m = (this->element->run(jt, end) || this->is_option);
+    bool m = (this->element->run(jt, end, r) || this->is_option);
 
     EXPR_MATCHED(m);
     return m;
@@ -204,6 +204,7 @@ bool abnf_vals::generate(str_const_iterator& it, const str_const_iterator& end)
     else if(jt != end && *jt == '%')
     {
         abnf_parser parser;
+        matched_patterns_t out;
         // spaces are omitted in some places to make the generation faster
         bool add = parser.add_rule("DIGIT = \"0\"|\"1\"|\"2\"|\"3\"|\"4\"|\"5\"|\"6\"|\"7\"|\"8\"|\"9\"");
         add = parser.add_rule("HEXDIG = DIGIT|\"A\"|\"B\"|\"C\"|\"D\"|\"E\"|\"F\"");
@@ -221,30 +222,30 @@ bool abnf_vals::generate(str_const_iterator& it, const str_const_iterator& end)
         add = parser.add_rule("num-val = bin-val|dec-val|hex-val");
         add = parser.generate("\"%\" num-val");
 
-        if(parser.run(jt, end))
+        if(parser.run(jt, end, out))
         {
             int num = -1;
-            if(!parser.get_rule("bin-val")->matched_pattern.empty())
+            if(!out["bin-val"].empty())
             {
                 assert(false); // bin not implemented
                 parser.add_rule("STR = 1*BIT");
                 num = 0;
             }
-            else if(!parser.get_rule("dec-val")->matched_pattern.empty())
+            else if(!out["dec-val"].empty())
             {
                 parser.add_rule("STR = 1*DIGIT");
                 num = 1;
             }
-            else if(!parser.get_rule("hex-val")->matched_pattern.empty())
+            else if(!out["hex-val"].empty())
             {
                 parser.add_rule("STR = 1*HEXDIG");
                 num = 2;
             }
             assert(num != -1);
 
-            if(!parser.get_rule("DOT")->matched_pattern.empty())
+            if(!out["DOT"].empty())
             {
-                std::string num_val = parser.get_rule("num-val")->matched_pattern;
+                std::string num_val = out["num-val"];
                 str_const_iterator kt = num_val.begin();
 
                 if(num == 0)
@@ -263,15 +264,16 @@ bool abnf_vals::generate(str_const_iterator& it, const str_const_iterator& end)
                     parser.add_rule("iterator = [\"x\"] STR [\".\"]");
                 }
 
-                while(parser.get_rule("iterator")->run(kt, num_val.end()))
+                out.clear();
+                while(parser.get_rule("iterator")->run(kt, num_val.end(), out))
                 {
                     unsigned int val;
                     std::stringstream sts;
 
                     if(num == 1)
-                        sts << std::dec << parser.get_rule("STR")->matched_pattern;
+                        sts << std::dec << out["STR"];
                     else if(num == 2)
-                        sts << std::hex << parser.get_rule("STR")->matched_pattern;
+                        sts << std::hex << out["STR"];
 
                     sts >> val;
                     this->char_val += (char)val;
@@ -282,9 +284,9 @@ bool abnf_vals::generate(str_const_iterator& it, const str_const_iterator& end)
                 EXPR_MATCHED(true);
                 return true;
             }
-            else if(!parser.get_rule("DASH")->matched_pattern.empty())
+            else if(!out["DASH"].empty())
             {
-                std::string num_val = parser.get_rule("num-val")->matched_pattern;
+                std::string num_val = out["num-val"];
                 str_const_iterator kt = num_val.begin();
 
                 if(num == 0)
@@ -305,14 +307,15 @@ bool abnf_vals::generate(str_const_iterator& it, const str_const_iterator& end)
 
                 int i = 0;
                 unsigned int val[2];
-                while(parser.get_rule("iterator")->run(kt, num_val.end()))
+                out.clear();
+                while(parser.get_rule("iterator")->run(kt, num_val.end(), out))
                 {
                     std::stringstream sts;
 
                     if(num == 1)
-                        sts << std::dec << parser.get_rule("STR")->matched_pattern;
+                        sts << std::dec << out["STR"];
                     else if(num == 2)
-                        sts << std::hex << parser.get_rule("STR")->matched_pattern;
+                        sts << std::hex << out["STR"];
 
                     sts >> val[i % 2];
                     i++;
@@ -330,9 +333,9 @@ bool abnf_vals::generate(str_const_iterator& it, const str_const_iterator& end)
                 std::stringstream sts;
                 unsigned int val;
                 if(num == 1)
-                    sts << std::dec << parser.get_rule("DECSTR")->matched_pattern;
+                    sts << std::dec << out["DECSTR"];
                 else if(num == 2)
-                    sts << std::hex << parser.get_rule("HEXSTR")->matched_pattern;
+                    sts << std::hex << out["HEXSTR"];
                 sts >> val;
 
                 this->type = RANGE_VAL;
@@ -346,14 +349,16 @@ bool abnf_vals::generate(str_const_iterator& it, const str_const_iterator& end)
     else if(jt != end && *jt == '<')
     {
         abnf_parser parser;
+        matched_patterns_t out;
         bool add = parser.add_rule("STR = *(%x20-3D|%x3F-7E)");
         add = parser.generate("\"<\" STR \">\"");
 
-        if(parser.run(jt, end))
+        if(parser.run(jt, end, out))
         {
+            assert(!out["STR"].empty());
             this->type = CHAR_VAL;
             this->sensitive = false;
-            this->char_val = parser.get_rule("STR")->matched_pattern;
+            this->char_val = out["STR"];
             EXPR_MATCHED(true);
             return true;
         }
@@ -362,7 +367,7 @@ bool abnf_vals::generate(str_const_iterator& it, const str_const_iterator& end)
     return false;
 }
 
-bool abnf_vals::run(str_const_iterator& it, const str_const_iterator& end)
+bool abnf_vals::run(str_const_iterator& it, const str_const_iterator& end, matched_patterns_t& r) const
 {
     str_const_iterator jt = it;
     if(this->type == CHAR_VAL)
@@ -396,7 +401,8 @@ abnf_rulename::abnf_rulename(abnf_parser& parser) :
 {
 }
 
-bool abnf_rulename::generate_rulename(str_const_iterator& it, const str_const_iterator& end)
+bool abnf_rulename::generate_rulename(
+    str_const_iterator& it, const str_const_iterator& end, std::string& rulename)
 {
     str_const_iterator jt = it, kt = it;
     if(jt == end || !IS_ALPHA(*jt))
@@ -413,7 +419,7 @@ bool abnf_rulename::generate_rulename(str_const_iterator& it, const str_const_it
         EXPR_MATCHED(true);
     }
 
-    this->rulename.assign(kt, it);
+    rulename.assign(kt, it);
 
     return true;
 }
@@ -421,12 +427,14 @@ bool abnf_rulename::generate_rulename(str_const_iterator& it, const str_const_it
 bool abnf_rulename::generate(str_const_iterator& it, const str_const_iterator& end)
 {
     str_const_iterator jt = it;
-    if(!this->generate_rulename(jt, end))
+
+    std::string rulename;
+    if(!this->generate_rulename(jt, end, rulename))
         return false;
 
     this->rule = NULL;
     for(auto kt = this->parser.rules.begin(); kt != this->parser.rules.end(); kt++)
-        if(kt->rulename == this->rulename)
+        if(kt->rulename == rulename)
         {
             this->rule = &(*kt);
             break;
@@ -438,12 +446,12 @@ bool abnf_rulename::generate(str_const_iterator& it, const str_const_iterator& e
     return true;
 }
 
-bool abnf_rulename::run(str_const_iterator& it, const str_const_iterator& end)
+bool abnf_rulename::run(str_const_iterator& it, const str_const_iterator& end, matched_patterns_t& r) const
 {
     assert(this->rule);
 
     str_const_iterator jt = it;
-    bool m = this->rule->run(jt, end);
+    bool m = this->rule->run(jt, end, r);
 
     EXPR_MATCHED(m);
     return m;
@@ -527,13 +535,13 @@ bool abnf_repetition::generate(str_const_iterator& it, const str_const_iterator&
     return false;
 }
 
-bool abnf_repetition::run(str_const_iterator& it, const str_const_iterator& end)
+bool abnf_repetition::run(str_const_iterator& it, const str_const_iterator& end, matched_patterns_t& r) const
 {
     str_const_iterator jt = it;
 
     if(!this->has_repeat)
     {
-        bool m = this->element.run(jt, end);
+        bool m = this->element.run(jt, end, r);
         EXPR_MATCHED(m);
         return m;
     }
@@ -541,7 +549,7 @@ bool abnf_repetition::run(str_const_iterator& it, const str_const_iterator& end)
     {
         // TODO: decide if use size_t instead of int
         int count = 0;
-        while(this->element.run(jt, end)) count++;
+        while(this->element.run(jt, end, r)) count++;
         
         if(count < this->repetitions.first 
             || (count > this->repetitions.second && this->repetitions.second != -1))
@@ -584,16 +592,16 @@ bool abnf_concatenation::generate(str_const_iterator& it, const str_const_iterat
     return true;
 }
 
-bool abnf_concatenation::run(str_const_iterator& it, const str_const_iterator& end)
+bool abnf_concatenation::run(str_const_iterator& it, const str_const_iterator& end, matched_patterns_t& r) const
 {
     str_const_iterator jt = it;
 
-    if(!this->left.run(jt, end))
+    if(!this->left.run(jt, end, r))
         return false;
     else
     {
         for(auto kt = this->right.begin(); kt != this->right.end(); kt++)
-            if(!kt->run(jt, end))
+            if(!kt->run(jt, end, r))
                 return false;
     }
 
@@ -635,10 +643,10 @@ bool abnf_alternation::generate(str_const_iterator& it, const str_const_iterator
     return true;
 }
 
-bool abnf_alternation::run(str_const_iterator& it, const str_const_iterator& end)
+bool abnf_alternation::run(str_const_iterator& it, const str_const_iterator& end, matched_patterns_t& r) const
 {
     str_const_iterator jt = it;
-    if(this->left.run(jt, end))
+    if(this->left.run(jt, end, r))
     {
         EXPR_MATCHED(true);
         return true;
@@ -647,7 +655,7 @@ bool abnf_alternation::run(str_const_iterator& it, const str_const_iterator& end
     {
         for(auto kt = this->right.begin(); kt != this->right.end(); kt++)
         {
-            if(kt->run(jt, end))
+            if(kt->run(jt, end, r))
             {
                 EXPR_MATCHED(true);
                 return true;
@@ -657,10 +665,11 @@ bool abnf_alternation::run(str_const_iterator& it, const str_const_iterator& end
     return false;
 }
 
-abnf_rule::abnf_rule(abnf_parser& parser) : 
+abnf_rule::abnf_rule(abnf_parser& parser, bool store_matched) : 
     parser(parser), 
     alternation(parser),
-    generated(false), matched(false)
+    generated(false),
+    store_matched(store_matched)
 {
 }
 
@@ -672,9 +681,10 @@ bool abnf_rule::generate(str_const_iterator& it, const str_const_iterator& end)
 
     // validates and stores the rulename
     abnf_rulename rulename(this->parser);
-    if(!rulename.generate_rulename(jt, end))
+    std::string rulename_str;
+    if(!rulename.generate_rulename(jt, end, rulename_str))
         return false;
-    this->rulename = rulename.rulename;
+    this->rulename = rulename_str;
 
     // defined-as
     while(consume_c_wsp(jt, end));
@@ -702,29 +712,30 @@ bool abnf_rule::generate(str_const_iterator& it, const str_const_iterator& end)
     return true;
 }
 
-bool abnf_rule::run(str_const_iterator& it, const str_const_iterator& end)
+bool abnf_rule::run(str_const_iterator& it, const str_const_iterator& end, matched_patterns_t& out) const
 {
     assert(this->generated);
     // TODO: incremental
     str_const_iterator jt = it;
-    this->matched = this->alternation.run(jt, end);
+    bool matched = this->alternation.run(jt, end, out);
 
-    if(this->matched)
-        this->matched_pattern.assign(it, jt);
+    // store matched pattern and bind it to rulename
+    if(this->store_matched && matched)
+        out[this->rulename].assign(it, jt);
 
-    EXPR_MATCHED(this->matched);
-    return this->matched;
+    EXPR_MATCHED(matched);
+    return matched;
 }
 
-abnf_parser::abnf_parser() : entry(*this)
+abnf_parser::abnf_parser() : entry(*this, false)
 {
 }
 
-bool abnf_parser::add_rule(std::string syntax)
+bool abnf_parser::add_rule(std::string syntax, bool store_matched)
 {
     syntax += "\r\n";
 
-    abnf_rule rule(*this);
+    abnf_rule rule(*this, store_matched);
     if(!rule.generate(syntax.begin(), syntax.end()))
         return false;
     this->rules.push_back(rule);
@@ -741,17 +752,25 @@ bool abnf_parser::generate(const std::string& syntax)
     return this->entry.generate(entry_syntax.begin(), entry_syntax.end());
 }
 
-bool abnf_parser::run(const std::string& input)
+bool abnf_parser::run(const std::string& input, matched_patterns_t& r) const
 {
-    return this->run(input.begin(), input.end());
+    return this->run(input.begin(), input.end(), r);
 }
 
-bool abnf_parser::run(str_const_iterator& it, const str_const_iterator& end)
+bool abnf_parser::run(str_const_iterator& it, const str_const_iterator& end, matched_patterns_t& r) const
 {
-    return this->entry.run(it, end);
+    return this->entry.run(it, end, r);
 }
 
 abnf_rule* abnf_parser::get_rule(const std::string& rulename)
+{
+    for(auto it = this->rules.begin(); it != this->rules.end(); it++)
+        if(it->rulename == rulename)
+            return &(*it);
+    return NULL;
+}
+
+const abnf_rule* abnf_parser::get_rule(const std::string& rulename) const
 {
     for(auto it = this->rules.begin(); it != this->rules.end(); it++)
         if(it->rulename == rulename)
